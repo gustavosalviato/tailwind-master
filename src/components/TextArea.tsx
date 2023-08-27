7d74f56d3c6b26dab526a141830315242f02fefe
gustavosalviato <gustavosalviato8@gmail.com>
2023-08-27 17:28:26 -0300

    chore: TextArea component

---
+++ src/components/TextArea.tsx
@@ -0,0 +1,12 @@
+import { ComponentProps } from 'react'
+
+type TextAreaProps = ComponentProps<'textarea'>
+
+export function TextArea(props: TextAreaProps) {
+  return (
+    <textarea
+      className="col-span-2 min-h-[120px] resize-y rounded-lg border border-zinc-300 p-4 px-3 py-2 text-zinc-900 shadow-sm"
+      {...props}
+    />
+  )
+}
